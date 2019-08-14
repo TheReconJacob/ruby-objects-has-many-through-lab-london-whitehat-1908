22e91985a870fc43e4692fef46c1361ae29e516d
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-14 11:19:25 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,12 +12,12 @@ class Patient
     @@all
   end
 
-  def new_appointment(doctor, date)
-    Appointment.new(date, self, doctor)
-  end
-
   def appointments
     Appointment.all.select { |appointment| appointment.patient == self }
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
   end
 
   def doctors
